2b3901e508240f690a3f853d961cd9532fc53e08
Sarkhan Adigozalov <89109894+SarkhanAdigozalov@users.noreply.github.com>
2024-07-05 19:40:41 +0300

    react-native-first-app

---
+++ App.js
@@ -0,0 +1,29 @@
+import Ders1 from './src/screens/Ders1';
+import Ornek1 from './src/components/Ornek1';
+import { TextInput, View } from 'react-native';
+import StateNedir from './src/components/StateNedir';
+import Alistirma1 from './src/screens/Alistirma1';
+import Ders2 from './src/screens/Ders2';
+import MyList from './src/components/MyList';
+// import Home from './src/screens/Home';
+
+const App = () => {
+  return (
+  <View>
+    {/* <Home />
+    <Ders1 
+      buttonTitle="1. Buton" 
+      placeholderText="1. metin girme alanı"
+      bacgroundColor="red"
+      placeholderTextColor="white"
+    />
+    <Ornek1 text1="özgür bunu yapar" text2="sdalkn" text3="fsghdfj" text4="ali haydar"/> 
+    <StateNedir />
+    <Alistirma1 />
+    <Ders2 /> */}
+    <MyList />
+  </View>
+  );
+}
+
+export default App
